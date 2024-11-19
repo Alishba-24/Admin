469c091fa940265d27b3b1c1b4f226868a56c35d
Alishba <alishbamalik3689@gmail.com>
2024-11-19 16:26:14 +0500

    create this new service file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Admin from './Pages/Admin';
 import AdminPage from './Pages/AdminPage'; 
 
 import { useSelector } from 'react-redux';
+import Service from './Pages/Service';
 
 const App = () => {
   const role = useSelector((state) => state.auth.role); // Get the role from Redux store
@@ -20,6 +21,7 @@ const App = () => {
 
         <Route path="/user" element={<User />} />
         <Route path="/userpage" element={<UserPage />} />
+        <Route path="/userpage" element={<Service />} />
       </Routes>
     </Router>
   );
